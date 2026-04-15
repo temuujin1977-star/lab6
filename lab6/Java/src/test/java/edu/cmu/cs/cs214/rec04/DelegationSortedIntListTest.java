614f1477373faf3cb1f34f6cc827bc3d54cd6aea
amarbold1357yahoocom <amarbold1357yahoo.com@gmail.com>
2026-04-15 15:22:50 +0800

    Lab 6 Java кодуудыг бүрэн нэмэв

---
+++ lab6/Java/src/test/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntListTest.java
@@ -0,0 +1,85 @@
+package edu.cmu.cs.cs214.rec04;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the DelegationSortedIntList class.
+ *
+ * @author Nora Shoemaker
+ */
+public class DelegationSortedIntListTest {
+
+    private DelegationSortedIntList list1;
+    private DelegationSortedIntList list2;
+
+    /**
+     * Called before each test.
+     */
+    @Before
+    public void setUp() {
+        list1 = new DelegationSortedIntList();
+        list2 = new DelegationSortedIntList();
+    }
+
+    /**
+     * Tests for the add() method.
+     */
+    @Test
+    public void testAdd() {
+        // add 5 elements to our list.
+        list1.add(1);
+        list1.add(3);
+        list1.add(2);
+        list1.add(4);
+        list1.add(2);
+        
+        // check that the total number of elements added is 5.
+        assertEquals(5, list1.getTotalAdded());
+        
+        // helper function that prints out the contents of an IntegerList
+        printList(list1);
+    }
+
+    /**
+     * Test for the addAll() method.
+     */
+    @Test
+    public void testAddAll() {
+        // add 5 elements to our first list.
+        list1.add(1);
+        list1.add(3);
+        list1.add(2);
+        list1.add(4);
+        list1.add(2);
+
+        // check that the total number of elements added is 5.
+        assertEquals(5, list1.getTotalAdded());
+
+        // add 2 elements to a second list.
+        list2.add(3);
+        list2.add(0);
+
+        // check that the total number of elements added is 2.
+        assertEquals(2, list2.getTotalAdded());
+
+        // add the first list (5 elements) to our second list (2 elements).
+        list2.addAll(list1);
+
+        // check that the total number of elements added to our second list is 7.
+        assertEquals(7, list2.getTotalAdded());
+    }
+
+    /**
+     * A helper function that prints out the contents of an IntegerList.
+     * @param list IntegerList to be printed out.
+     */
+    private void printList(IntegerList list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i));
+            System.out.print(", ");
+        }
+        System.out.println();
+    }
+}
---
+++ lab6/Java/src/test/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntListTest.java
@@ -0,0 +1,83 @@
+package edu.cmu.cs.cs214.rec04;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the InheritanceSortedIntList class.
+ *
+ * @author Nora Shoemaker
+ */
+public class InheritanceSortedIntListTest {
+
+    private InheritanceSortedIntList list1;
+    private InheritanceSortedIntList list2;
+
+    /**
+     * Called before each test.
+     */
+    @Before
+    public void setUp() {
+        list1 = new InheritanceSortedIntList();
+        list2 = new InheritanceSortedIntList();
+    }
+
+    /**
+     * Tests for the add() method.
+     */
+    @Test
+    public void testAdd() {
+        // add 5 elements to our list.
+        list1.add(1);
+        list1.add(3);
+        list1.add(2);
+        list1.add(4);
+        list1.add(2);
+        
+        // check that the total number of elements added is 5.
+        assertEquals(5, list1.getTotalAdded());
+        printList(list1);
+    }
+
+    /**
+     * Tests for the addAll() method.
+     */
+    @Test
+    public void testAddAll() {
+        // add 5 elements to our first list.
+        list1.add(1);
+        list1.add(3);
+        list1.add(2);
+        list1.add(4);
+        list1.add(2);
+
+        // check that the total number of elements added is 5.
+        assertEquals(5, list1.getTotalAdded());
+
+        // add 2 elements to a second list.
+        list2.add(3);
+        list2.add(0);
+
+        // check that the total number of elements added is 2.
+        assertEquals(2, list2.getTotalAdded());
+
+        // add the first list (5 elements) to our second list (2 elements).
+        list2.addAll(list1);
+
+        // check that the total number of elements added to our second list is 7.
+        assertEquals(7, list2.getTotalAdded());
+    }
+
+    /**
+     * A helper function that prints out the contents of an IntegerList.
+     * @param list IntegerList to be printed out.
+     */
+    private void printList(IntegerList list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i));
+            System.out.print(", ");
+        }
+        System.out.println();
+    }
+}
